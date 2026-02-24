79dd0587fd711219ff4d80d408cd7f72d766dbed
Kit Langton <kit.langton@gmail.com>
2026-02-24 16:40:20 -0500

    Add Solid adapter and align animation behavior with React

---
+++ src/solid/index.ts
@@ -0,0 +1,35 @@
+// Core API
+export { createDialKit } from './createDialKit';
+export type { CreateDialOptions } from './createDialKit';
+
+// Root component
+export { DialRoot } from './components/DialRoot';
+export type { DialPosition } from './components/DialRoot';
+
+// Component exports
+export { Slider } from './components/Slider';
+export { Toggle } from './components/Toggle';
+export { Folder } from './components/Folder';
+export { ButtonGroup } from './components/ButtonGroup';
+export { SpringControl } from './components/SpringControl';
+export { SpringVisualization } from './components/SpringVisualization';
+export { TextControl } from './components/TextControl';
+export { SelectControl } from './components/SelectControl';
+export { ColorControl } from './components/ColorControl';
+export { PresetManager } from './components/PresetManager';
+
+// Store exports
+export { DialStore } from '../store/DialStore';
+export type {
+  SpringConfig,
+  ActionConfig,
+  SelectConfig,
+  ColorConfig,
+  TextConfig,
+  Preset,
+  DialValue,
+  DialConfig,
+  ResolvedValues,
+  ControlMeta,
+  PanelConfig,
+} from '../store/DialStore';
